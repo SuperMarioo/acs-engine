7e0060a7ddb8e961cf7891a02cd1423ff3c296d0
Mariusz Serwin <mariooserwin@gmail.com>
2018-09-21 14:29:51 +0100

    Update register-dns.sh

--- register-dns.sh
+++ register-dns.sh
@@ -5,22 +5,6 @@ set -e
 
 DOMAINNAME=$1
 
-echo $(date) " - Starting Script"
-
-cat > /etc/network/if-up.d/register-dns <<EOF
-#!/bin/sh
-
-    ip=\$(ip address show eth0 | awk '/inet / {print \$2}' | cut -d/ -f1)
-    host=\$(hostname -s)
-    
-    sed -i '/'\$host'/d' /etc/hosts
-
-    sed  -i -e '\$a\'\$ip'  '\$host'' /etc/hosts
-    
-EOF
-
-chmod 755 /etc/network/if-up.d/register-dns
-
 if ! grep -Fq "${DOMAINNAME}" /etc/dhcp/dhclient.conf
 then
     echo $(date) " - Adding domain to dhclient.conf"
